6cd277b2fd409856825be7bafb63fedf208b505c
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-09-26 12:44:14 +0100

    Removed duplicate email from Contact

--- src/Contact.js
+++ src/Contact.js
@@ -13,8 +13,6 @@ return (
         If you have a role you would like to discuss, a general enquiry or
         would just like to have a chat please email me at d3bbie.dann@gmail.com.
       </p>
-      <p className="text-xl mb-6 text-teal-800">Email: d3bbie.dann@gmail.com</p>
-
       <p className="text-xl mb-6 text-teal-800">
         You can also find me on Twitter for a chat, check out my GitHub, or connect on LinkedIn for something a
         bit more professional. Come say hello.
